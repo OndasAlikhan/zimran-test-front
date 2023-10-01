d05ca158a72395e27f9d27454688f59694fb407c
Alikhan Ondas <alihan.ondas@gmail.com>
2023-10-01 21:45:40 +0600

    config: add env

--- src/store/chatApi.ts
+++ src/store/chatApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const chatApi = createApi({
   reducerPath: "chats",
   tagTypes: ["Chats", "Messages"],
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL }),
   endpoints: (build) => ({
     getChats: build.query({
       query: () => `chats`,
